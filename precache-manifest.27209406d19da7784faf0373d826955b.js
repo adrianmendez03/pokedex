c830d32c2630fc3b7286fa4afbcfe55d8a0cdca0
Adrian Mendez <adrianmndz328@gmail.com>
2020-06-26 16:14:45 -0500

    Updates

---
+++ precache-manifest.27209406d19da7784faf0373d826955b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e262691a223b78743644d821af4100fc",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "fc17a9ec9ba1a8b99061",
+    "url": "/pokedex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "df9cdd4bb6eda0954fa6",
+    "url": "/pokedex/static/js/2.4072d654.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.4072d654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc17a9ec9ba1a8b99061",
+    "url": "/pokedex/static/js/main.406ba0db.chunk.js"
+  },
+  {
+    "revision": "ed5a9742913badf7a85c",
+    "url": "/pokedex/static/js/runtime-main.82622ded.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokedex/static/media/logo.5d5d9eef.svg"
+  }
+]);
